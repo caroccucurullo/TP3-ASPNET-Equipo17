2702e6e8e97379658e8a6521938df99f95ac1810
Gnieva94 <gus_94@hotmail.com.ar>
2023-06-05 17:28:34 -0300

    Default back : limpieza de comentarios. Cambios en event OnTextChange para cargar listas filtradas.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -15,7 +15,6 @@ namespace TP3_ASPNET_Equipo17
     {
 
         public List<Articulo> ListaArticulos { get; set; }
-        //public List<Imagenes> Imagenes { get; set; }
         public List<Imagenes> ListaPrimerasImagenes { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -24,26 +23,15 @@ namespace TP3_ASPNET_Equipo17
                 if (!IsPostBack)
                 {
                     ArticuloNegocio negocio = new ArticuloNegocio();
-                    Session.Add("ListaArticulos", negocio.listar());
-
                     ListaArticulos = negocio.listar();
-                    //dgvArticulos.DataSource = negocio.listar();
-                    //dgvArticulos.DataBind();
-                    // aspRepeater.DataSource = ListaArticulos;
-                    // aspRepeater.DataBind();
-
                     ImagenesNegocio imagenesNegocio = new ImagenesNegocio();
                     List<Imagenes> listaImagenesSql = imagenesNegocio.listar();
                     ListaPrimerasImagenes = new List<Imagenes>();
                     foreach (Articulo item in ListaArticulos)
                     {
-
                         ListaPrimerasImagenes.Add(listaImagenesSql.Find(x => x.IdArticulo == item.Id));
-
                     }
-
-
-
+                    Session.Add("ListaArticulos", ListaArticulos);
                     Session.Add("PrimerasImagenes", ListaPrimerasImagenes);
 
                     if (Session["Carrito"] == null)
@@ -51,31 +39,47 @@ namespace TP3_ASPNET_Equipo17
                         Carrito carrito = new Carrito();
                         Session.Add("Carrito", carrito);
                     }
-
                 }
                 ListaPrimerasImagenes = (List<Imagenes>)Session["PrimerasImagenes"];
                 ListaArticulos = (List<Articulo>)Session["ListaArticulos"];
-
-
             }
             catch (Exception ex)
             {
                 Session.Add("error", ex);
                 throw;
             }
-           
-           
-
         }
 
         protected void txtFiltro_TextChanged(object sender, EventArgs e)
         {
-            List<Articulo> lista = (List<Articulo>)Session["ListaArticulos"];
-            List<Articulo> listaFiltrada = lista.FindAll(x => x.Nombre.ToUpper().Contains(txtFiltro.Text.ToUpper()));
+            List<Imagenes> listaImagenes = (List<Imagenes>)Session["PrimerasImagenes"];
+            List<Articulo> listaFiltrada = ((List<Articulo>)Session["ListaArticulos"]).FindAll(x => x.Nombre.ToUpper().Contains(txtFiltro.Text.ToUpper()));
+            List<Imagenes> listaImagenesFiltrada = new List<Imagenes>();
+            foreach (Articulo item in listaFiltrada)
+            {
+                foreach (var img in listaImagenes)
+                {
+                    if (img != null)
+                    {
+                        if (img.IdArticulo == item.Id)
+                        {
+                            listaImagenesFiltrada.Add(img);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Imagenes vacio = new Imagenes()
+                        {
+                            IdArticulo = item.Id,
+                            ImagenUrl = ""
+                        };
+                        listaImagenesFiltrada.Add(vacio);
+                    }
+                }
+            }
             ListaArticulos = listaFiltrada;
-         
-    
-
+            ListaPrimerasImagenes = listaImagenesFiltrada;
         }
 
     }
--- Default.aspx.designer.cs
+++ Default.aspx.designer.cs
@@ -22,14 +22,5 @@ namespace TP3_ASPNET_Equipo17
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtFiltro;
-
-        /// <summary>
-        /// Control divCards.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divCards;
     }
 }
